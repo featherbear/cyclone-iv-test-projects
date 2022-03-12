7f5605e30d43eddf87bdc7ffd4a1534b8d1eefb2
Andrew Wong <andrew.j.wong@outlook.com>
2022-03-13 03:32:13 +1100

    I need a pinout diagram reeeee

--- counter-demo.vhd
+++ counter-demo.vhd
@@ -13,18 +13,19 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY counter_demo IS
 	PORT (
-		SEG: OUT STD_LOGIC_VECTOR(0 TO 11)
+		SEG: OUT STD_LOGIC_VECTOR(0 TO 11);
+		KEY1: IN STD_LOGIC;
+		KEY2: IN STD_LOGIC;
+		KEY3: IN STD_LOGIC;
+		KEY4: IN STD_LOGIC
 	);
 END counter_demo;
 
 ARCHITECTURE structure OF counter_demo IS
 	SIGNAL a, b, c, d : std_logic_vector(3 DOWNTO 0);
-BEGIN
-	a <= "1111";
 	
-	h: ENTITY work.part_3461BS PORT MAP (
-		HEX0 => '1' & '1' & a,
-		PIN => SEG
-	);
+BEGIN
+	SEG <= KEY1 & KEY2 & KEY3 & KEY4 & "00000000";
+	
 	
 END structure;	
