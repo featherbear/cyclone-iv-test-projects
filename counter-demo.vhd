1b6859aa70f2a55270d650a7d3a929855d1e2cb2
Andrew Wong <andrew.j.wong@outlook.com>
2022-03-15 03:20:41 +1100

    multiplexer seems to be broken rn

--- counter-demo.vhd
+++ counter-demo.vhd
@@ -13,7 +13,8 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY counter_demo IS
 	PORT (
-		SEG: OUT STD_LOGIC_VECTOR(0 TO 11);
+	   CLK: IN STD_LOGIC;
+		SEG: OUT STD_LOGIC_VECTOR(11 DOWNTO 0);
 		KEY1: IN STD_LOGIC;
 		KEY2: IN STD_LOGIC;
 		KEY3: IN STD_LOGIC;
@@ -25,7 +26,11 @@ ARCHITECTURE structure OF counter_demo IS
 	SIGNAL a, b, c, d : std_logic_vector(3 DOWNTO 0);
 	
 BEGIN
-	SEG <= KEY1 & KEY2 & KEY3 & KEY4 & "00000000";
+	disp: ENTITY work.PART_3461BS PORT MAP (
+	   CLK => clk,
+	   HEX0 =>"11" & KEY1 & KEY2 & KEY3 & KEY4,
+		PIN => SEG
+	);
 	
 	
 END structure;	
--- part_3461BS.vhd
+++ part_3461BS.vhd
@@ -3,19 +3,24 @@ USE ieee.std_logic_1164.ALL;
 
 ENTITY PART_3461BS IS
     PORT (
-        HEX0 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '1');
-        HEX1 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '1');
-        HEX2 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '1');
-        HEX3 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '1');
+	     CLK  : IN STD_LOGIC;
+        HEX0 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '0');
+        HEX1 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := "100001"; --(OTHERS => '0');
+        HEX2 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := "000010"; --(OTHERS => '0');
+        HEX3 : IN STD_LOGIC_VECTOR(5 DOWNTO 0) := "100011"; --(OTHERS => '0');
         PIN : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
     );
 END PART_3461BS;
 
 ARCHITECTURE func OF PART_3461BS IS
-    SIGNAL SEG_DISPLAY : STD_LOGIC_VECTOR(6 DOWNTO 0);
-    SIGNAL SEG_DOT : STD_LOGIC;
+    SIGNAL SEG_DISPLAY : STD_LOGIC_VECTOR(6 DOWNTO 0); -- Set to 0 to show segment
+    SIGNAL SEG_DOT : STD_LOGIC; -- Set to 0 to show dot
 	 
     SIGNAL SEG_N : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1000";
+	 
+	 SIGNAL tick : STD_LOGIC := '0';
+	 
+	 constant holdTime : integer := 80000;
 BEGIN
     PIN(0) <= SEG_DISPLAY(2);
     PIN(1) <= SEG_DISPLAY(3);
@@ -30,23 +35,37 @@ BEGIN
     PIN(10) <= SEG_DISPLAY(6);
     PIN(11) <= SEG_N(3);
 
-    PROCESS (HEX0) IS
-        VARIABLE NEXT_VALUE : STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '1');
+	 PROCESS
+		  VARIABLE counter : natural range 0 to holdTime := 0;
+	 BEGIN
+	     WAIT UNTIL rising_edge(clk);
+		  counter := counter + 1;
+		  IF counter = holdTime THEN
+		      tick <= NOT(tick);
+		  END IF;
+	 END PROCESS;
+	 
+    PROCESS IS
+        VARIABLE NEXT_VALUE : STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '0');
+		  VARIABLE NEXT_VALUE_BUFFER : STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '0');
     BEGIN
+	     WAIT UNTIL rising_edge(tick);
+		  
         SEG_N <= SEG_N(0) & SEG_N(3 DOWNTO 1);
-        SEG_DISPLAY <= (OTHERS => '1');
+
 
         CASE SEG_N IS
-            WHEN "1000" => NEXT_VALUE := HEX0;
-            WHEN "0100" => NEXT_VALUE := HEX1;
-            WHEN "0010" => NEXT_VALUE := HEX2;
-            WHEN "0001" => NEXT_VALUE := HEX3;
-				WHEN OTHERS => NEXT_VALUE := (OTHERS => '1');
+            WHEN "1000" => NEXT_VALUE_BUFFER := HEX0;
+            WHEN "0100" => NEXT_VALUE_BUFFER := HEX1;
+            WHEN "0010" => NEXT_VALUE_BUFFER := HEX2;
+            WHEN "0001" => NEXT_VALUE_BUFFER := HEX3;
+				WHEN OTHERS => NEXT_VALUE_BUFFER := (OTHERS => '0');
         END CASE;
+		  
 		  
         -- Clockwise from top, then horizontal bar
         IF NEXT_VALUE(5) = '1' THEN
-			  SEG_DOT <= NEXT_VALUE(4);
+			  SEG_DOT <= NOT(NEXT_VALUE(4)); -- Set input(4) to 1 to show
 			  CASE NEXT_VALUE(3 DOWNTO 0) IS
 					WHEN "0000" => SEG_DISPLAY <= "0000001"; -- 0
 					WHEN "0001" => SEG_DISPLAY <= "1001111"; -- 1
@@ -65,7 +84,11 @@ BEGIN
 					WHEN "1110" => SEG_DISPLAY <= "0110000"; -- E
 					WHEN "1111" => SEG_DISPLAY <= "0111000"; -- F
 			  END CASE;
+		  ELSE
+			  SEG_DISPLAY <= (OTHERS => '1');
+			  SEG_DOT <= '1';
 		  END IF;
 		  
+		  NEXT_VALUE := NEXT_VALUE_BUFFER;
     END PROCESS;
 END func;
